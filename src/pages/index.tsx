8e760aa401eb1f4021b0ea446ca38a7cefcd5403
azkal <mohazkalarif182@gmail.com>
2023-03-26 03:46:35 +0700

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoPlayer from '../components/VideoPlayer';
 
 const MyPage = () => {
-  const videoUrl = 'https://www850.ff-02.com/token=NUq9-RByk4G0CrFYj-M36g/1679790243/139.194.0.0/186/f/eb/55ce76ba50bfe94bbbd14c65ccda9ebf-1080p.mp4'
+  const videoUrl = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
   return (
     <div>
       <h1>My Page</h1>
